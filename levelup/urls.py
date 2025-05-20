a4de05dbd5f2ed63abc7e79a54f830c8caa2d183
Alyssa Cleland <alyssamariecleland@gmail.com>
2025-05-19 22:22:17 -0500

    get single and get all fotr game typed, games, events

--- levelup/urls.py
+++ levelup/urls.py
@@ -16,7 +16,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import include
+from rest_framework import routers
+from levelupapi.views import GameTypeView, EventView, GameView
 
+
+# The trailing_slash=False tells the router to accept /gametypes instead of /gametypes/. It’s a very annoying error to come across, when your server is not responding and the code looks right, the only issue is your fetch url is missing a / at the end.
+router = routers.DefaultRouter(trailing_slash=False)
+# The next line is what sets up the /gametypes resource (see more with visual: https://github.com/nashville-software-school/server-side-python-curriculum/blob/evening-cohorts/book-3-levelup/chapters/LU_LIST_RETRIEVE.md). more below @ bottom
+router.register(r'gametypes', GameTypeView, 'gametype')
+router.register(r'events', EventView, 'event')
+# This line is added to register the GameView
+router.register(r'games', GameView, 'game')
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),  # add router variable to urlpatterns
 ]
+
+
+# The first parameter, r'gametypes, is setting up the url.
+# The second GameTypeView is telling the server which view to use when it sees that url.
+# The third, gametype, is called the base name. You’ll only see the base name if you get an error in the server. It acts as a nickname for the resource and is usually the singular version of the url.
